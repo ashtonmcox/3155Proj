e7203fcfcb3bd8251b41fed8073e65b669a03d78
ashtonmcox <168942375+ashtonmcox@users.noreply.github.com>
2024-11-30 13:01:55 -0500

    fixed resources model nullable values

--- api/models/resources.py
+++ api/models/resources.py
@@ -9,9 +9,9 @@ class Resource(Base):
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name = Column(String(512), nullable=False)
-    type = Column(String(512))
-    amount = Column(DECIMAL(10, 2))
-    unit = Column(String(128))
+    type = Column(String(512), nullable=False)
+    amount = Column(DECIMAL(10, 2), nullable=False)
+    unit = Column(String(128), nullable=False)
 
     menu_items_recipes = relationship(
         "Recipes", secondary="menu_items_recipes_resources", back_populates="resources"
