7f2d16e1010411d4ab4d1e23bc0c51325cae464e
ashtonmcox <168942375+ashtonmcox@users.noreply.github.com>
2024-11-30 13:20:56 -0500

    updated orders schema

--- api/models/orders.py
+++ api/models/orders.py
@@ -4,15 +4,19 @@ from datetime import datetime
 from ..dependencies.database import Base
 
 
+
+
 class Order(Base):
-    __tablename__ = "orders"
+   __tablename__ = "orders"
 
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey("users.id"))
-    status = Column(String(512), nullable=False)
-    date = Column(DateTime, default=datetime.utcnow)
-    tracking_number = Column(String(128))
-    total_price = Column(DECIMAL(10, 2))
 
-    user = relationship("User", back_populates="orders")
-    details = relationship("OrderDetail", back_populates="order")
+   id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+   user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+   status = Column(String(512), nullable=False)
+   date = Column(DateTime, default=datetime.utcnow)
+   tracking_number = Column(String(128), nullable=False)
+   total_price = Column(DECIMAL(10, 2), nullable=False)
+
+
+   user = relationship("User", back_populates="orders")
+   details = relationship("OrderDetail", back_populates="order")
