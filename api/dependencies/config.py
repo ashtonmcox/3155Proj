21b04333615309175f57424ae2f2f41a1445d8a6
Connor Schwab <Connor.l.schwab@gmail.com>
2024-12-04 14:50:13 -0500

    feat: fixed users

--- api/dependencies/config.py
+++ api/dependencies/config.py
@@ -1,8 +1,8 @@
 class conf:
     db_host = "localhost"
-    db_name = "restaurant_api"
+    db_name = "sandwich_maker_api"
     db_port = 3306
     db_user = "root"
-    db_password = "Asforl!fe1"
+    db_password = "Coolkidz1234!"
     app_host = "localhost"
     app_port = 8000
